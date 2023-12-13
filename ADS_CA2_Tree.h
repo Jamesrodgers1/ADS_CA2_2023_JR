f6aee1c2ffd95fe4767c83a15b133d06eb7bd6c8
Jamesrodgers1 <d00237812@student.dkit.ie>
2023-12-13 00:23:22 +0000

    tried to implement the rest but code broke

--- ADS_CA2_Tree.h
+++ ADS_CA2_Tree.h
@@ -1,20 +1,20 @@
-
 #ifndef ADS_CA2_TREE_H
 #define ADS_CA2_TREE_H
 
 #include <string>
 #include <vector>
 #include <stack>
+#include <queue> 
 #include "tinyxml2.h"
-
 
 class TreeNode {
 public:
     std::string name;
     bool isFolder;
     std::vector<TreeNode*> children;
+    int size; // Size in bytes for files, not used for folders
 
-    TreeNode(const std::string& name, bool folder);
+    TreeNode(const std::string& name, bool folder, int size = 0);
     ~TreeNode();
 };
 
@@ -24,17 +24,22 @@ private:
 
     TreeNode* findNode(TreeNode* node, const std::string& name);
     void display(TreeNode* node, int level = 0);
+    
 
 public:
     FileTree(TreeNode* rootNode);
     ~FileTree();
-    void addNode(const std::string& parentFolder, const std::string& name, bool isFolder);
+    void addNode(const std::string& parentFolder, const std::string& name, bool isFolder, int size = 0);
     TreeNode* getNode(const std::string& name);
     void listAll(TreeNode* node, int level = 0);
     TreeNode* search(TreeNode* node, const std::string& name);
     void displayTree();
+    int calculateFolderSize(const std::string& folderName);
+    int calculateFolderSizeBFS(const std::string& folderName);
+    int countItemsInFolder(const std::string& folderName);
+
+    // Placeholder for XML validation and parsing
+    bool validateAndParseXML(tinyxml2::XMLElement* element, TreeNode* parentNode, FileTree& fileTree, std::stack<std::string>& tags);
 };
 
-bool validateAndParseXML(tinyxml2::XMLElement* element, TreeNode* parentNode, FileTree& fileTree, std::stack<std::string>& tags);
-
-#endif 
+#endif // ADS_CA2_TREE_H
--- Ads_CA2_Tree.cpp
+++ Ads_CA2_Tree.cpp
@@ -1,56 +1,156 @@
 #include "ADS_CA2_Tree.h"
 #include "tinyxml2.h"
+#include <iostream>
 
 // TreeNode functions
-TreeNode::TreeNode(const std::string& name, bool folder) : name(name), isFolder(folder) {
+TreeNode::TreeNode(const std::string& name, bool folder, int size)
+    : name(name), isFolder(folder), size(size), children() {
     // Constructor logic
 }
 
 TreeNode::~TreeNode() {
-    // Destructor logic
+    for (auto* child : children) {
+        delete child;
+    }
 }
 
 // FileTree functions
-FileTree::FileTree(TreeNode* rootNode) : root() {
+FileTree::FileTree(TreeNode* rootNode) : root(rootNode) {
     // Constructor logic
 }
 
 FileTree::~FileTree() {
-    // Destructor logic
+    delete root;
 }
 
 TreeNode* FileTree::findNode(TreeNode* node, const std::string& name) {
     // Logic to find a node
-    return nullptr; // Placeholder
+    if (node == nullptr) return nullptr;
+    if (node->name == name) return node;
+
+    for (auto* child : node->children) {
+        TreeNode* result = findNode(child, name);
+        if (result != nullptr) {
+            return result;
+        }
+    }
+
+    return nullptr;
 }
 
 void FileTree::display(TreeNode* node, int level) {
-    // Logic to display the tree
+    if (node == nullptr) return;
+
+    std::cout << std::string(level * 2, ' ') << node->name << std::endl;
+    for (auto* child : node->children) {
+        display(child, level + 1);
+    }
 }
 
-void FileTree::addNode(const std::string& parentFolder, const std::string& name, bool isFolder) {
-    // Logic to add a node
+void FileTree::addNode(const std::string& parentFolder, const std::string& name, bool isFolder, int size) {
+    TreeNode* parentNode = findNode(this->root, parentFolder);
+    if (parentNode && parentNode->isFolder) {
+        TreeNode* newNode = new TreeNode(name, isFolder, size);
+        parentNode->children.push_back(newNode);
+    }
+    else {
+        std::cerr << "Parent folder not found or is not a folder: " << parentFolder << std::endl;
+    }
 }
 
 TreeNode* FileTree::getNode(const std::string& name) {
-    // Logic to get a node
-    return nullptr; // Placeholder
+    return findNode(root, name);
 }
 
 void FileTree::listAll(TreeNode* node, int level) {
-    // Logic to list all nodes
+    display(node, level);
 }
 
 TreeNode* FileTree::search(TreeNode* node, const std::string& name) {
-    // Logic to search for a node
-    return nullptr; // Placeholder
+    return findNode(node, name);
 }
 
 void FileTree::displayTree() {
-    // Logic to display the entire tree
+    display(root, 0);
 }
 
-bool validateAndParseXML(tinyxml2::XMLElement* element, TreeNode* parentNode, FileTree& fileTree, std::stack<std::string>& tags) {
-    // Logic to validate and parse XML
-    return true; // Placeholder
+int FileTree::calculateFolderSize(const std::string& folderName) {
+    // implementation
+}
+
+int FileTree::countItemsInFolder(const std::string& folderName) {
+    // implementation
+}
+bool FileTree::validateAndParseXML(tinyxml2::XMLElement* element, TreeNode* parentNode, FileTree& fileTree, std::stack<std::string>& tags) {
+    if (!element) return false;
+
+    while (element) {
+        std::string tagName = element->Value();
+        std::string name = element->Attribute("name");
+        bool isFolder = tagName == "folder";
+        int size = isFolder ? 0 : std::atoi(element->Attribute("size"));
+
+        if (tagName != "folder" && tagName != "file") {
+            std::cerr << "Invalid tag: " << tagName << std::endl;
+            return false;
+        }
+
+        if (name.empty()) {
+            std::cerr << "Missing name attribute in " << tagName << std::endl;
+            return false;
+        }
+
+        if (!isFolder && size <= 0) {
+            std::cerr << "Invalid or missing size for file: " << name << std::endl;
+            return false;
+        }
+
+        tags.push(tagName);
+
+        TreeNode* newNode = new TreeNode(name, isFolder, size);
+        parentNode->children.push_back(newNode);
+
+        if (isFolder) {
+            if (!validateAndParseXML(element->FirstChildElement(), newNode, fileTree, tags)) {
+                return false;
+            }
+
+            if (tags.top() != "folder") {
+                std::cerr << "Tag mismatch. Expected closing tag for folder." << std::endl;
+                return false;
+            }
+
+            tags.pop();
+        }
+
+        element = element->NextSiblingElement();
+    }
+
+    return true;
+}
+
+int FileTree::calculateFolderSizeBFS(const std::string& folderName) {
+    TreeNode* startNode = getNode(folderName);
+    if (!startNode || !startNode->isFolder) {
+        return -1; // Return -1 if not a folder or node not found
+    }
+
+    std::queue<TreeNode*> queue;
+    queue.push(startNode);
+
+    int totalSize = 0;
+    while (!queue.empty()) {
+        TreeNode* currentNode = queue.front();
+        queue.pop();
+
+        if (!currentNode->isFolder) {
+            totalSize += currentNode->size;
+        }
+
+        for (auto* child : currentNode->children) {
+            queue.push(child);
+        }
+    }
+
+    return totalSize;
 }
